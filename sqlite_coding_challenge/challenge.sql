7e2083bfe3465d91acf0774258ce44b9eceb4cb8
Wyatt Mullen <mullenw@usf.edu>
2025-11-14 15:39:52 -0500

    Corrected Group By to Order By and made small name changes to Joins

--- sqlite_coding_challenge/challenge.sql
+++ sqlite_coding_challenge/challenge.sql
@@ -1,12 +1,14 @@
 --Task ONE
-SELECT c.first_name, c.last_name, SUM(oi.quantity * oi.unit_price) AS total_spent
+SELECT c.first_name || ' ' || c.last_name AS customer_name, SUM(oi.quantity * oi.unit_price) AS total_spend
 FROM customers c
-Join orders o ON o.customr_id = c.customer_id
-JOIN order_items oi ON o.order_id = oi.order_id
-Group BY total_spent DESC
+Join orders o ON o.customer_id = c.id
+JOIN order_items oi ON oi.order_id = o.id
+Group BY c.id
+ORDER BY total_spend DESC
 LIMIT 5;
 
+
 --Task TWO
-SELECT p.category, (p.price - oi.unit_price) * oi.quantity AS Revenue
+SELECT p.category, (p.price - oi.unit_price) AS Revenue
 FROM order_items oi
 Group By p.category
