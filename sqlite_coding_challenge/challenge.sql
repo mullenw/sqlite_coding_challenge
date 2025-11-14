cd582b399aad7e9fd9b6f36c46fe62d7fdacf079
Wyatt Mullen <mullenw@usf.edu>
2025-11-14 16:16:18 -0500

    fixed syntax to all capital letters for queries for task 1

--- sqlite_coding_challenge/challenge.sql
+++ sqlite_coding_challenge/challenge.sql
@@ -1,9 +1,9 @@
 --Task ONE
 SELECT c.first_name || ' ' || c.last_name AS customer_name, SUM(oi.quantity * oi.unit_price) AS total_spend
 FROM customers c
-Join orders o ON o.customer_id = c.id
+JOIN orders o ON o.customer_id = c.id
 JOIN order_items oi ON oi.order_id = o.id
-Group BY c.id
+GROUP BY c.id
 ORDER BY total_spend DESC
 LIMIT 5;
 
